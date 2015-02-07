ac24d8d4a3ba26320510c4770dac9985c79fef1f
Pieplu <alexis.pieplu@gmail.com>
2015-02-07 13:29:32 -0500

    remove phone

--- sidebar-footer-third.php
+++ sidebar-footer-third.php
@@ -8,7 +8,6 @@
             echo '<div class="widget widget_text">';
             echo '<h5>Contact</h5>';
             echo '<div class="textwidget">';
-            echo 'Téléphone (Président): <a href="tel:+1418 522-2028">418 522-2028</a></br>';
             echo 'e-mail: <a href="mailto:info@linuq.org">info@linuq.org</a></div>';
             echo '</div>';
         }
